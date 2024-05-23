bc34da9c10a59c01739b23538226c85f2f7e8fdd
Arun Kumar R <aruntutter19@gmail.com>
2024-05-23 12:21:37 +0530

    supported browser section created

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Banner from "./components/Banner";
 import Review from "./components/Review";
 import MagicWand from "./components/MagicWand";
 import ApplyCard from "./components/ApplyCard";
+import SupportedBrowsers from "./components/SupportedBrowsers";
 
 const App = () => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const App = () => {
       <Review />
       <MagicWand />
       <ApplyCard />
+      <SupportedBrowsers />
     </div>
   );
 };
