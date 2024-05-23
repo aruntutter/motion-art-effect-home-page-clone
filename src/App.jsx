798bfe84ce0e9667a79b3b75f589b7c489bc8dd4
Arun Kumar R <aruntutter19@gmail.com>
2024-05-23 10:26:56 +0530

    WebGL fluid simulation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import initFluid from "./script";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    initFluid();
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-8xl font-bold text-pink-400">Motion Art Effect</h1>
